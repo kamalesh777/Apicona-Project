a36f5b167200ee259327c3aa2d0a96668ecceb9a
Kamalesh777 <ksmaity777@gmail.com>
2020-11-05 21:48:21 +0530

    Before Footer Part

--- js/script.js
+++ js/script.js
@@ -36,3 +36,23 @@ $('.blogSlider').owlCarousel({
         }
     }
 })
+
+//Team Slider
+$('.teamSlider').owlCarousel({
+    loop:true,
+    margin:30,
+    responsiveClass: true,
+    nav: true,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items: 4,
+        }
+    }
+})
